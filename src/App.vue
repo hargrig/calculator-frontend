853ab0581ba615dd828ee4fbc57961ad32d9cd49
Grig <gog.harutyunyan@gmail.com>
2019-01-20 20:57:56 +0400

    Initial Commit: Simple calculator app: VueJS

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/calc.png">
+		<Calculator msg="calculate  what  you  need" /> 
+	</div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+	name: 'app',
+	components: {
+		Calculator
+	}
+}
+</script>
+
+<style>
+body {
+	background-color: #16141A;
+}
+
+#app {
+	font-family: 'Rajdhani', sans-serif;
+	text-align: center;
+	margin-top: 60px;
+}
+</style>
